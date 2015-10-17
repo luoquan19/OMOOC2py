432c6d26bf95c042cad08993dbbbb2a427eff498
luoquan19 <42454021@qq.com>
2015-10-18 00:02:21 +0800

    Update main.py
    节奏101

---
+++ _src/om2py0w/0wex0/main.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# Quick Python Script Explanation for Programmers
+
+import os
+
+def main():
+    print 'hello world!'
+    print "This is Alice's greeting."
+    print 'This is Bob\'s greeting.'
+    
+    foo(5,10)
+    
+    print '='*10
+    print 'Current working directory is ' + os.getcwd()
+    
+    counter = 0
+    counter += 1
+    
+    food = [ 'apples'; 'oranges’; ‘cats']
+    
+    for i in food: 
+        print 'I like to eat' + i
+        
+      print 'Count to ten：'
+      for i in range(10):
+        print i
+      
+    def foo(param1, secondParam）:
+      res = param1 + secondParam
+      
+      print '%s plus %s is equal to %s’ %
+      
+      if res < 50:
+        print 'foo'
+        
+      elif (res >= 50) end ((param1== 42) or (secondParam == 24)):
+        print 'bar'
+        
+      else:
+        print 'moo'
+        
+      return res # This is a one-line comment.
+      ''' A multi-
+  line string , but also be a multi-line comment.'''
+  
+  if _name_ == '_main_’
+  
